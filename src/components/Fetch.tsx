2c335040b39a0921698ce63534b2d5ce609ca678
abolfazl-edalati <a.g.e.tech@outlook.com>
2026-08-11 07:58:37 +0330

    fetch data with async await

--- src/components/Fetch.tsx
+++ src/components/Fetch.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -11,10 +11,23 @@ const Fetch = () => {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data))
-      .catch((err) => setError(err.message));
+    // axios
+    //   .get<User[]>("https://jsonplaceholder.typicode.com/users")
+    //   .then((res) => setUsers(res.data))
+    //   .catch((err) => setError(err.message));
+
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "https://jsonplaceholder.typicode.com/users",
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
