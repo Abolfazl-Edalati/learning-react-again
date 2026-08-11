163376b75e457fb3e863d07e88b9cbef71792822
abolfazl-edalati <a.g.e.tech@outlook.com>
2026-08-11 07:40:48 +0330

    use axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import { useState } from "react";
-import UseEffect from "./components/UseEffect";
+import Fetch from "./components/Fetch";
 
 function App() {
-  const [category, setCategory] = useState<string>("");
-
-  return (
-    <>
-      <div className="m-2">
-        <select
-          name=""
-          className="form-select"
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value=""></option>
-          <option value="Clothing">Clothing</option>
-          <option value="Household">Household</option>
-        </select>
-        <UseEffect category={category} />
-      </div>
-    </>
-  );
+  return <Fetch />;
 }
 
 export default App;
