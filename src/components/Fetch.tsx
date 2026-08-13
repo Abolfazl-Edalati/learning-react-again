901a345e81428b3627490efdfa9bdcbb742dfe99
abolfazl-edalati <a.g.e.tech@outlook.com>
2026-08-13 16:54:15 +0330

    axios delete

--- src/components/Fetch.tsx
+++ src/components/Fetch.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -9,33 +9,69 @@ interface User {
 const Fetch = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    // axios
-    //   .get<User[]>("https://jsonplaceholder.typicode.com/users")
-    //   .then((res) => setUsers(res.data))
-    //   .catch((err) => setError(err.message));
+    const controller = new AbortController();
 
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          "https://jsonplaceholder.typicode.com/users",
-        );
+    setLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
         setUsers(res.data);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
 
-    fetchData();
+    return () => controller.abort();
+
+    // const fetchData = async () => {
+    //   try {
+    //     const res = await axios.get(
+    //       "https://jsonplaceholder.typicode.com/users",
+    //     );
+    //     setUsers(res.data);
+    //   } catch (err) {
+    //     setError((err as AxiosError).message);
+    //   }
+    // };
+    // fetchData();
   }, []);
+
+  const onDelete = (user: User) => {
+    const originalUsers = [...users];
+
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
 
   return (
     <div>
-      <p className="text-danger">{error}</p>
-      <ul>
+      {error && <p className="text-red-700 mb-5">{error}</p>}
+      {isLoading && <span className="spinner-border"></span>}
+      <ul className="list-none space-y-2">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="flex justify-between max-w-2xs">
+            {user.name}
+            <button
+              className=" border p-2 rounded-xl border-red-500 text-red-600 font-semibold cursor-pointer"
+              onClick={() => onDelete(user)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
